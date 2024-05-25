fdb6e454ee0a2904387432ce825bb4337e5675f1
rhaeldms <rhaeldms@gmail.com>
2024-05-24 20:28:06 -0400

    ola ser humano

--- src/main/java/br/ce/wcaquino/taskbackend/controller/RootController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/RootController.java
@@ -11,14 +11,6 @@ public class RootController {
 	
 	@GetMapping
 	public String hello() {
-		return 			"0000000     0000000     L         UU       UU    IIIII    ZZZZZZZ    AAAAA  \n" + \
-						"0000000000 0000000000   L         UU       UU     III         ZZ     AAAAAA \n" + \
-						"000000000000000000000   L         UU       UU     III       ZZ       AA   AA\n" + \
-						" 0000000000000000000    L         UU       UU     III      ZZ        AAAAAAA\n" + \
-						"   0000000000000000     L         UU       UU     III     ZZ         AA   AA\n" + \
-						"     000000000000       LLLLLLL    UUUUUUUUU      III     ZZZZZZZ    AA    AA\n" + \
-						"       00000000       \n" + \
-						"         0000        \n" +  \
-						"          00          ";
+		return "Olá ser humano!";
 	}
 }
