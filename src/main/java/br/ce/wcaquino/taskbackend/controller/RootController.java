d3d736be5b572f50cff4747c38fde1b01d01ce71
rhaeldms <rhaeldms@gmail.com>
2024-05-24 20:10:17 -0400

    retirando a luiza

--- src/main/java/br/ce/wcaquino/taskbackend/controller/RootController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/RootController.java
@@ -13,30 +13,4 @@ public class RootController {
 	public String hello() {
 		return "Olá humano!";
 	}
-	public class ParabensAmigaLuiza {
-		public static void main(String[] args) {
-			String nome = "Luiza";
-			String mensagem = gerarMensagem(nome);
-			System.out.println(mensagem);
-		}
-	
-		private static String gerarMensagem(String nome) {
-			StringBuilder sb = new StringBuilder();
-			
-			sb.append("****************************************\n");
-			sb.append("*                                      *\n");
-			sb.append("*        PARABÉNS, AMIGA ").append(nome.toUpperCase()).append("!        *\n");
-			sb.append("*                                      *\n");
-			sb.append("****************************************\n");
-			sb.append("*                                      *\n");
-			sb.append("*    Desejo a você um dia maravilhoso! *\n");
-			sb.append("*    Que todos os seus sonhos se       *\n");
-			sb.append("*    realizem e que você seja muito    *\n");
-			sb.append("*    feliz hoje e sempre.              *\n");
-			sb.append("*                                      *\n");
-			sb.append("****************************************\n");
-	
-			return sb.toString();
-		}
-	}
 }
